faaa441e32f6740a7f9abf42d4a0ab28f5247852
Michael Wilburn <michael-mw@hotmail.com>
2023-05-15 21:10:34 -0300

    Visibilidad de metodos y atributos, estructura de paquetes,Herencia y polimorfismo

---
+++ cap02/src/libro/cap02/misclases/FechaDetallada.java
@@ -0,0 +1,23 @@
+package libro.cap02.misclases;
+
+import libro.cap02.fechas.Fecha;
+
+public class FechaDetallada extends Fecha {
+    private static String meses[] = {
+            "Enero","Febrero","Marzo",
+            "Abril","Mayo","Junio",
+            "Julio","Agosto","Septiembre",
+            "Octubre","Noviembre","Diciembre"
+    };
+
+    public String toString(){
+      return getDia()+" de "+meses[getMes()-1]+" de "+getAnio();
+    };
+
+    public FechaDetallada(String s) {
+        super(s);
+    }
+
+    public FechaDetallada() {
+    }
+}
